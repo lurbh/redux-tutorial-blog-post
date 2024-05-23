44eff6883a83b4db3de517411afeebae7eeec679
lurbh <53887968+lurbh@users.noreply.github.com>
2024-05-23 17:05:37 +0800

    first commit

--- src/store/posts/reducer.js
+++ src/store/posts/reducer.js
@@ -20,17 +20,58 @@ const postsSlice = createSlice({
     },
     reducers: {
         /** @param {{type: string, payload: Post} action} */
-        createPost: function (state, action) {
-            // TODO: Add the new post into the state
+        createPost: {
+            prepare: function(input) {
+                return {
+                    payload : {
+                        id: nanoid(),
+                        createdAt: Date.now(),
+                        updatedAt: Date.now(),
+                        title: input.title,
+                        content: input.content,
+                    }
+                }
+            },
+            reducer: function (state, action) {
+                state.value.push({
+                    id: action.payload.id,
+                    createdAt: action.payload.createdAt,
+                    updatedAt: action.payload.updatedAt,
+                    title: action.payload.title,
+                    content: action.payload.content,
+                })
+            }
         },
 
         /** @param {{type: string, payload: Post} action} */
-        editPost: function (state, action) {
-            // TODO: Find the post by id, then update its contents
+        editPost: {
+            prepare: function(input) {
+                return {
+                    payload : {
+                        ...input,
+                        updatedAt: Date.now()
+                    }
+                }
+            },
+            reducer: function (state, action) {
+                const post = state.value.find((p) => p.id === action.payload.id)
+                if(!post)
+                {
+                    throw new Error("Cannot Find post for ID " + action.payload.id);
+                }
+                post.content = action.payload.content;
+                post.title = action.payload.title;
+                post.updatedAt = action.payload.updatedAt;
+            }
         },
 
-        deletePost: function (state, action) {
-           // TODO: Find the post by id, then delete it from state
+        deletePost:  function (state, action) {
+            const index = state.value.findIndex((p) => p.id === action.payload.id)
+            if(index < 0)
+            {
+                throw new Error("Cannot Find post for ID " + action.payload.id);
+            }
+            state.value.splice(index,1)
         }
     }
 });
@@ -40,6 +81,12 @@ export const selectAllPosts = () => (state) => state.posts.value;
 export const selectPostById = (id) => (state) => {
     // TODO: Complete this - to find a post by its ID
     // Recall that `state` refers to the root state
+    const post = state.posts.value.find((post) => post.id === id)
+    if(post < 0)
+    {
+        throw new Error("Cannot Find post for ID " + id);
+    }
+    return post;
 }
 
 // These will be used by downstream modules when dispatching actions to the store
